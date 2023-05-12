16c22a42ea8ba019b929f6aea4d086b062eb7f3c
DereviankoYroslav <yarik0009258@gmail.com>
2023-05-12 14:08:45 +0300

    Initial Commit

---
+++ test-task/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import firtsImg from './images/first-screen-image.svg'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <img className='first-screen-image' src={firtsImg}></img>
+      
+    </div>
+  );
+}
+
+export default App;
