21cfc2c037b15f29ba113817d83c0d474c0021dd
DereviankoYroslav <yarik0009258@gmail.com>
2023-05-15 22:34:10 +0300

    +Header

--- test-task/src/components/Header.jsx
+++ test-task/src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../images/logo.svg'
 
 const Header = () => {
   return (
     <div className='header'>
-        <p>Header</p>
+        <div className='header-row'>
+            <div className='logo'>
+                <img src={logo}></img>
+            </div>
+            <div className='item-header'>
+                <Link className='header-text'>Home</Link>
+                <Link className='header-text'>About</Link>
+                <Link className='header-text'>Products</Link>
+                <Link className='header-text'>Use Cases</Link>
+                <Link className='header-text'>Pricing</Link>
+                <Link className='header-text'>Resources</Link>
+            </div>
+        </div>
     </div>
   )
 }
